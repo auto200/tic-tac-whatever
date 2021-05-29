b3b17741f2db3ece4f333e60207f6eccdc486fc9
auto200 <michal.warac@gmail.com>
2021-05-29 15:22:30 +0200

    Add types

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
 import theme from "../utils/theme";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
