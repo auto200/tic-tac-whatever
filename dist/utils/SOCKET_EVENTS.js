284cd5ac7cf0ebd874d33fc81707133b04367e08
auto200 <michal.warac@gmail.com>
2021-06-20 13:42:31 +0200

    Ignore dist

--- dist/utils/SOCKET_EVENTS.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SOCKET_EVENTS = void 0;
-var SOCKET_EVENTS;
-(function (SOCKET_EVENTS) {
-    SOCKET_EVENTS["GAME_UPDATE"] = "GAME_UPDATE";
-})(SOCKET_EVENTS = exports.SOCKET_EVENTS || (exports.SOCKET_EVENTS = {}));
